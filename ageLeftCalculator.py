f2971716e29c5364965f863730435cd8e4d4acf5
AmirSaleh <95437656+vabede@users.noreply.github.com>
2022-06-07 15:46:36 -0400

    Update ageLeftCalculator.py

--- ageLeftCalculator.py
+++ ageLeftCalculator.py
@@ -3,20 +3,13 @@
 # print("your name has " + new_num_char + " characters." )
 # print(type(num_char))
 # 🚨 Age Calculator 👇
-age = input("What is your current age?") 👆
+age = input("What is your current age?") 
 # age input type is str
 # change the type of age to int
 ageinInt=int(age) 
-currentAgeMonth=ageinInt*12
-currentAgeWeeks=ageinInt*52
-currentAgeDays=ageinInt*365
-totalAgeMonth =90*12
-totalAgeDays=90*365
-totalAgeWeeks=90*52
-leftDays=totalAgeDays-currentAgeDays
-leftMonths=totalAgeMonth-currentAgeMonth
-leftWeeks=totalAgeWeeks-currentAgeWeeks
-leftDays=str(leftDays)
-leftMonths=str(leftMonths)
-leftWeeks=str(leftWeeks)
-print("You have "+leftDays+" days, "+leftWeeks+" weeks, and "+leftMonths+" months left if you live 90 years.")
+remain_age_years = 90-ageinInt
+leftDays=remain_age_years*365
+leftWeeks=remain_age_years*52
+leftMonths=remain_age_years*12
+message=f"You have {leftDays} days, {leftWeeks} weeks, and {leftMonths} months left."
+print(message)
